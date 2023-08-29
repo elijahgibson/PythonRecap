2982cabdb07ecf8b8c6c513f453e1c8c9bb9aa3e
Elijah Gibson <elijah_gibson1@baylor.edu>
2023-08-29 14:28:20 -0500

    updated gitignore file

--- MyGraph.py
+++ MyGraph.py
@@ -7,3 +7,6 @@ plt.plot(x, np.sin(x))
 plt.show()
 
 print("Hi there!")
+## not running, we need create a VE (bottom right), activate it (the green letters), install (in this case matplotlib, squiggly lines will disappear)
+## don't need to track the VE, gitignore "NAME/"
+## students don't submit latest version if it is not checked in, if files in source control, make sure to committ them
