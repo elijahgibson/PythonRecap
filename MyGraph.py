7679bff2b1504d34bc9d30a6afbb224411ce4f9b
Elijah Gibson <elijah_gibson1@baylor.edu>
2023-08-29 14:36:54 -0500

    removed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -6,7 +6,8 @@ plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
 ## not running, we need create a VE (bottom right), activate it (the green letters), install (in this case matplotlib, squiggly lines will disappear)
 ## don't need to track the VE, gitignore "NAME/"
 ## students don't submit latest version if it is not checked in, if files in source control, make sure to committ them
+## put in name of folder in gitignore with a backslash, then save it,
+## then source control, write a message and committ it
